282e797c1f1664c17eadec9f4b590a99a07052b6
Ustun Ertas <ustunertas0@gmail.com>
2022-05-24 22:24:13 +0200

    24.05.2022

---
+++ src/test/java/day14_excel/C02_ReadExcel.java
@@ -0,0 +1,28 @@
+package day14_excel;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C02_ReadExcel {
+
+    @Test
+    public void readExcelTest() throws IOException {
+
+        // 7. Dosya yolunu bir String degiskene atayalim
+        String dosyaYolu="src/resources/ulkeler.xlsx";
+
+        //8. FileInputStream objesi olusturup,parametre olarak dosya yolunu girelim
+        FileInputStream fis= new FileInputStream(dosyaYolu);
+
+        //9. Workbook objesi olusturalim,parameter olarak fileInputStream objesini girelim
+        Workbook workbook= WorkbookFactory.create(fis);
+
+        String actulaData = workbook.getSheet("Sayfa1").getRow(3).getCell(3).toString();
+
+    }
+}
